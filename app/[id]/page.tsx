3213733157fe4a8c1e7d93481258a9a130de353f
Bonnke Omorehiomwan <BonnkeWebs@gmail.com>
2025-08-26 01:21:07 +0100

    fix padding

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -49,7 +49,7 @@ const ProjectPage = ({ params }: { params: Promise<{ id: number }> }) => {
             transition={{ delay: 0.4, duration: 0.5 }}
           >
             {/* View Live Button */}
-            <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-[#000319] transform hover:-translate-y-1 transition duration-300">
+            <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] px-4 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-[#000319] transform hover:-translate-y-1 transition duration-300">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-[#0D0E2E] px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                 <Link
